2afd6bc53bcd340cd6094747e2b72d2f13a59b48
Andrew <57403896+andusha@users.noreply.github.com>
2024-04-23 20:41:31 +0700

    сменил ковычки

--- api/config.py
+++ api/config.py
@@ -5,15 +5,15 @@ class ApiKeySettings(BaseSettings):
     api_key: str
 
     class Config:
-        env_prefix = 'OPENAI_'
-        env_file = '.env'
-        extra = 'ignore'
+        env_prefix = "OPENAI_"
+        env_file = ".env"
+        extra = "ignore"
 
 
 class RedisSettings(BaseSettings):
     url: str
 
     class Config:
-        env_prefix = 'REDIS_'
-        env_file = '.env'
-        extra = 'ignore'
+        env_prefix = "REDIS_"
+        env_file = ".env"
+        extra = "ignore"
